fe9fce24aab486cd35a79bc4a969b94a92f3f140
Akshar Dave <akshar314@gmail.com>
2021-07-18 18:10:13 +0530

    using default route table

--- main.tf
+++ main.tf
@@ -34,19 +34,15 @@ resource "aws_internet_gateway" "myapp_igw" {
  }
 }
 
-# route table = virutal router
-resource "aws_route_table" "myapp-route-table" {
-  vpc_id = aws_vpc.myapp-vpc.id
-  route  {
+
+resource "aws_default_route_table" "main-rtb" {
+  default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
+   route  {
       cidr_block = "0.0.0.0/0"
       gateway_id = aws_internet_gateway.myapp_igw.id
   }
   tags = {
-    Name: "${var.env_prefix}-rtb"
+    Name: "${var.env_prefix}-main-rtb"
   }
-}
 
-resource "aws_route_table_association" "a-rtb-subnet" {
-  subnet_id = aws_subnet.myapp-subnet-1.id
-  route_table_id = aws_route_table.myapp-route-table.id
 }
