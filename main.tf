cdebc4e5eb391da05b2963ba9860f362e213149f
Akshar Dave <akshar314@gmail.com>
2021-08-07 23:38:59 +0530

    create ec2 instance

--- main.tf
+++ main.tf
@@ -8,6 +8,7 @@ variable subnet_cidr_block {}
 variable avail_zone {}
 variable env_prefix {}
 variable my_ip {}
+variable instance_type {}
 
 
 
@@ -55,7 +56,7 @@ resource "aws_default_security_group" "default-sg" {
     from_port = 22
     to_port = 22
     protocol = "tcp"
-    cidr_blocks = [var.my_ip]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
@@ -75,5 +76,40 @@ resource "aws_default_security_group" "default-sg" {
 
   tags  = {
     Name: "${var.env_prefix}-default-sg"
+  }
+}
+
+data "aws_ami" "latest-amazon-linux-image" {
+    most_recent = true
+    owners = ["amazon"]
+    filter {
+      name = "name"
+      values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+    }
+
+    filter {
+      name = "virtualization-type"
+      values = ["hvm"]
+    }
+}
+
+output "aws_ami_id" {
+  value = data.aws_ami.latest-amazon-linux-image.id
+}
+
+
+resource "aws_instance" "myapp-server" {
+  ami = data.aws_ami.latest-amazon-linux-image.id
+  instance_type = var.instance_type
+
+  subnet_id = aws_subnet.myapp-subnet-1.id
+  vpc_security_group_ids = [aws_default_security_group.default-sg.id]
+  availability_zone = var.avail_zone
+
+  associate_public_ip_address = true
+  key_name = "og-private"
+
+  tags = {
+    Name =  "${var.env_prefix}-server"
   }
 }
