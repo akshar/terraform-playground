b0dea46d84c9ea878db4fd2d0e506660d33f8e8f
Akshar Dave <akshar314@gmail.com>
2021-08-09 02:20:19 +0530

    more modules

--- main.tf
+++ main.tf
@@ -18,73 +18,13 @@ module "myapp-subnet" {
  default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
 }
 
-resource "aws_default_security_group" "default-sg" {
-  # name = "myapp-sg" use default name
-  vpc_id = aws_vpc.myapp-vpc.id
-
-  ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port = 8080
-    to_port = 8080
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-    prefix_list_ids = []
-  }
-
-  tags  = {
-    Name: "${var.env_prefix}-default-sg"
-  }
-}
-
-data "aws_ami" "latest-amazon-linux-image" {
-    most_recent = true
-    owners = ["amazon"]
-    filter {
-      name = "name"
-      values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-    }
-
-    filter {
-      name = "virtualization-type"
-      values = ["hvm"]
-    }
-}
-
-
-
-resource "aws_key_pair" "ssh-key" {
-  key_name = "server-key"
-  public_key = file(var.public_key_location)
-}
-
-
-resource "aws_instance" "myapp-server" {
-  ami = data.aws_ami.latest-amazon-linux-image.id
-  instance_type = var.instance_type
-
-  subnet_id = module.myapp-subnet.subnet.id
-  vpc_security_group_ids = [aws_default_security_group.default-sg.id]
-  availability_zone = var.avail_zone
-
-  associate_public_ip_address = true
-  key_name = aws_key_pair.ssh-key.key_name
-
-  user_data = file("entry-script.sh")
-
-  tags = {
-    Name =  "${var.env_prefix}-server"
-  }
+module "myapp-webserver" {
+source = "./modules/webserver"
+vpc_id = aws_vpc.myapp-vpc.id
+subnet_id = module.myapp-subnet.subnet.id
+avail_zone = var.avail_zone
+env_prefix = var.env_prefix
+public_key_location = var.public_key_location
+instance_type = var.instance_type
+image_name = "amzn2-ami-hvm-*-x86_64-gp2"
 }
