9b141b6dd7c6b0ffbfe7cc01a07b0a5823515738
Akshar Dave <akshar314@gmail.com>
2021-08-09 01:00:50 +0530

    convert to modules

--- main.tf
+++ main.tf
@@ -2,17 +2,6 @@ provider "aws" {
   region     = "ap-south-1"
 }
 
-
-variable vpc_cidr_block {}
-variable subnet_cidr_block {}
-variable avail_zone {}
-variable env_prefix {}
-variable my_ip {}
-variable instance_type {}
-variable public_key_location {}
-
-
-
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_block
   tags = {
@@ -20,33 +9,13 @@ resource "aws_vpc" "myapp-vpc" {
   }
 }
 
-resource "aws_subnet" "myapp-subnet-1" {
-  vpc_id            = aws_vpc.myapp-vpc.id
-  cidr_block        = var.subnet_cidr_block
-  availability_zone = var.avail_zone
-  tags = {
-    Name : "${var.env_prefix}-subnet-1"
-  }
-}
-
-#internet gateway = virtual modem
-resource "aws_internet_gateway" "myapp_igw" {
+module "myapp-subnet" {
+ source = "./modules/subnet"
+ subnet_cidr_block = var.subnet_cidr_block
+ avail_zone = var.avail_zone
+ env_prefix = var.env_prefix
  vpc_id = aws_vpc.myapp-vpc.id
- tags = {
-   Name: "${var.env_prefix}-igw"
- }
-}
-
-
-resource "aws_default_route_table" "main-rtb" {
-  default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
-   route  {
-      cidr_block = "0.0.0.0/0"
-      gateway_id = aws_internet_gateway.myapp_igw.id
-  }
-  tags = {
-    Name: "${var.env_prefix}-main-rtb"
-  }
+ default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
 }
 
 resource "aws_default_security_group" "default-sg" {
@@ -94,9 +63,7 @@ data "aws_ami" "latest-amazon-linux-image" {
     }
 }
 
-output "aws_ami_id" {
-  value = data.aws_ami.latest-amazon-linux-image.id
-}
+
 
 resource "aws_key_pair" "ssh-key" {
   key_name = "server-key"
@@ -108,7 +75,7 @@ resource "aws_instance" "myapp-server" {
   ami = data.aws_ami.latest-amazon-linux-image.id
   instance_type = var.instance_type
 
-  subnet_id = aws_subnet.myapp-subnet-1.id
+  subnet_id = module.myapp-subnet.subnet.id
   vpc_security_group_ids = [aws_default_security_group.default-sg.id]
   availability_zone = var.avail_zone
 
